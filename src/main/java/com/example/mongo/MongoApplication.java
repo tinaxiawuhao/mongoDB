b9c67eaaf9c445d80ae418969fa9e5a6ba824bdf
wuhao <18365298803@163.com>
2020-01-03 17:52:04 +0800

    GridFS使用

--- src/main/java/com/example/mongo/MongoApplication.java
+++ src/main/java/com/example/mongo/MongoApplication.java
@@ -1,7 +1,11 @@
 package com.example.mongo;
 
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.gridfs.GridFSBucket;
+import com.mongodb.client.gridfs.GridFSBuckets;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class MongoApplication {
@@ -9,5 +13,4 @@ public class MongoApplication {
     public static void main(String[] args) {
         SpringApplication.run(MongoApplication.class, args);
     }
-
 }
--- src/test/java/com/example/mongo/MongoApplicationTests.java
+++ src/test/java/com/example/mongo/MongoApplicationTests.java
@@ -1,13 +1,63 @@
 package com.example.mongo;
 
+
+import com.mongodb.client.gridfs.GridFSBucket;
+import com.mongodb.client.gridfs.GridFSDownloadStream;
+import com.mongodb.client.gridfs.model.GridFSFile;
+import org.apache.commons.io.IOUtils;
+import org.bson.types.ObjectId;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.gridfs.GridFsResource;
+import org.springframework.data.mongodb.gridfs.GridFsTemplate;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 @SpringBootTest
 class MongoApplicationTests {
 
+    @Autowired
+    GridFsTemplate gridFsTemplate;
+
+    @Autowired
+    GridFSBucket gridFSBucket;
+
     @Test
-    void contextLoads() {
+    public void queryFile() throws IOException {
+        //5c7f660608726733f47b3f78
+        String fileId = "5e0f0e61250ba450a487c934";
+        //根据id查询文件
+        GridFSFile gridFSFile = gridFsTemplate.findOne(Query.query(Criteria.where("_id").is(fileId)));
+        //打开下载流对象
+        GridFSDownloadStream gridFSDownloadStream = gridFSBucket.openDownloadStream(gridFSFile.getObjectId());
+        //创建gridFsResource，用于获取流对象
+        GridFsResource gridFsResource = new GridFsResource(gridFSFile, gridFSDownloadStream);
+        //获取流中的数据
+        String s = IOUtils.toString(gridFsResource.getInputStream(), "UTF-8");
+        System.out.println(s);
+
+    }
+
+    @Test
+    void contextLoads() throws FileNotFoundException {
+        File file = new File("D:\\素材图\\1.jpg");
+        FileInputStream inputStream = new FileInputStream(file);
+        //保存模版文件内容
+        ObjectId fileId  = gridFsTemplate.store(inputStream, "测试文件1","");
+        System.out.println(fileId);
+    }
+
+    //删除文件
+    @Test
+    public void testDelFile() throws IOException {
+        //根据文件id删除fs.files和fs.chunks中的记录
+        gridFsTemplate.delete(Query.query(Criteria.where("_id").is("5e0f0e61250ba450a487c934")));
     }
 
 }
