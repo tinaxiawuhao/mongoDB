dce1e89725f4f6cc9dee8bef586aee035c8ba677
wuhao <18365298803@163.com>
2020-01-11 16:22:44 +0800

    dev3分支提交

--- src/main/java/com/example/mongo/testdev3.java
+++ src/main/java/com/example/mongo/testdev3.java
@@ -1,5 +1,14 @@
 package com.example.mongo;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
 public class testdev3 {
-    //测试
+    private String three;
 }
